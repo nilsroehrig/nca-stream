e4ce66f73f133763c89f34a2e3a3805994d99071
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2024-08-26 08:32:25 +0200

    add layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import '@picocss/pico';
+
+	let { children } = $props();
+</script>
+
+<header class="container">
+	<nav>
+		<ul>
+			<li><strong>NCA Stream</strong></li>
+		</ul>
+	</nav>
+</header>
+
+<main class="container">
+	{@render children()}
+</main>
